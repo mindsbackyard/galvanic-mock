51c9f8cc8cde301bedb2f4b5fdf3af75c26cf114
Christopher Bacher <mindsbackyard@gmail.com>
2017-06-15 00:56:54 +0200

    change util::Singleton to singleton! -> default method call ONCE only for the first call regardless of implementor

--- src/util.rs
+++ src/util.rs
@@ -25,22 +25,46 @@ fn convert_token_to_syntax_stmts<'a>(cx: &'a mut ExtCtxt, tokens: Vec<quote::Tok
     MacEager::stmts(SmallVector::many(stmts))
 }
 
-pub trait Singleton: Default + Clone {
-    fn singleton() -> Self {
-        use std::sync::{Once, ONCE_INIT};
-        // Initialize it to a null value
-        static mut SINGLETON: *const i8 = 0 as *const i8;
-        static ONCE: Once = ONCE_INIT;
+#[macro_export]
+macro_rules! singleton {
+    ( $type_name: ident ) => {
+        impl $type_name {
+            pub fn singleton() -> Self {
+                use::std::mem;
+                use std::sync::{Once, ONCE_INIT};
+                // Initialize it to a null value
+                static mut SINGLETON: *const i8 = 0 as *const i8;
+                static ONCE: Once = ONCE_INIT;
 
-        unsafe {
-            ONCE.call_once(|| {
-                let data = Self::default();
-                // Put it in the heap so it can outlive this call
-                let ptr: *const Self = mem::transmute(Box::new(data));
-                SINGLETON = mem::transmute(ptr);
-            });
-            let ptr: *const Self = mem::transmute(SINGLETON);
-            (*ptr).clone()
+                unsafe {
+                    ONCE.call_once(|| {
+                        let data = Self::default();
+                        // Put it in the heap so it can outlive this call
+                        let ptr: *const Self = mem::transmute(Box::new(data));
+                        SINGLETON = mem::transmute(ptr);
+                    });
+                    let ptr: *const Self = mem::transmute(SINGLETON);
+                    (*ptr).clone()
+                }
+            }
         }
+    }
+}
+
+#[macro_export]
+macro_rules! get_singleton {
+    ( $var_name: ident of $type_name: ident ) => {
+        let singleton = $type_name::singleton();
+        let gate = singleton.inner.lock();
+        let $var_name = gate.unwrap();
+    }
+}
+
+#[macro_export]
+macro_rules! get_singleton_mut {
+    ( $var_name: ident of $type_name: ident ) => {
+        let singleton = $type_name::singleton();
+        let gate = singleton.inner.lock();
+        let mut $var_name = gate.unwrap();
     }
 }
