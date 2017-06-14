727b69394bff21383028963e3063dc911977b035
Christopher Bacher <mindsbackyard@gmail.com>
2017-06-15 01:02:48 +0200

    refactoring in lib.rs change to use singleton! remove buggy implementation for handling raw strings during parsing of macros

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@
 #![allow(warnings)]
 #![recursion_limit = "128"]
 
+#[macro_use] mod util;
 mod mock_definition;
 mod new_mock;
 mod given;
 mod generate_mocks;
 mod data;
-mod util;
 
 #[cfg(test)]#[macro_use]
 extern crate galvanic_assert;
@@ -42,7 +42,6 @@ use new_mock::handle_new_mock;
 use given::handle_given;
 use generate_mocks::handle_generate_mocks;
 use data::*;
-use util::Singleton;
 
 
 #[plugin_registrar]
@@ -65,26 +64,23 @@ fn handle_then_verify_interactions(source: &str, absolute_position: usize) -> (S
 fn has_balanced_quotes(source: &str)  -> bool {
     let mut count = 0;
     let mut skip = false;
-    let mut is_raw = true;
     for c in source.chars() {
         if skip {
             skip = false;
             continue;
         }
 
-        if c == 'r' && count % 2 == 0 {
-            is_raw = true;
-        } else if c == '\\' && !is_raw {
+        if c == '\\' {
             skip = true;
         } else if c == '\"' {
             count += 1;
         }
-
-        is_raw = false;
+        //TODO handle raw strings
     }
     count % 2 == 0
 }
 
+/// Stores position of a macro invocation with the variant naming the macro.
 pub enum MacroInvocationPos {
     NewMock(usize),
     Given(usize),
@@ -92,9 +88,17 @@ pub enum MacroInvocationPos {
     ThenVerifyInteractions(usize)
 }
 
+/// Find the next galvanic-mock macro invocation in the source string.
+///
+/// Looks for `new_mock!``, `given!`, `expect_interactions!`, and `then_verify_interactions!`.
+/// The `source` string must have been reassembled from a `TokenTree`.
+/// The `source` string is expected to start in a code context, i.e., not inside
+/// a string.
 fn find_next_mock_macro_invocation(source: &str) -> Option<MacroInvocationPos> {
     use MacroInvocationPos::*;
+    // there must be a space between the macro name and the ! as the ! is a separate token in the tree
     let macro_names = ["new_mock !", "given !", "expect_interactions !", "then_verify_interactions !"];
+    // not efficient but does the job
     macro_names.into_iter()
                .filter_map(|&mac| {
                             source.find(mac).and_then(|pos| {
@@ -113,71 +117,62 @@ fn find_next_mock_macro_invocation(source: &str) -> Option<MacroInvocationPos> {
                 }))
 }
 
-fn prepare_handle_macro<F>(source: &str, mac_pos_relative_to_source: usize, absolute_pos_of_source: usize, handler: F) -> (String, usize, String)
+fn handle_macro<F>(source: &str, mac_pos_relative_to_source: usize, absolute_pos_of_source: usize, handler: F) -> (String, usize, String)
 where F: Fn(&str, usize) -> (String, String) {
     let absolute_pos_of_mac = absolute_pos_of_source + mac_pos_relative_to_source;
 
-    let (left, right) = source.split_at(mac_pos_relative_to_source);
-    let (mut generated, unhandled) = handler(right, absolute_pos_of_mac);
-    generated.push_str(&unhandled);
+    let (left_of_mac, right_with_mac) = source.split_at(mac_pos_relative_to_source);
+    let (mut generated_source, unhandled_source) = handler(right_with_mac, absolute_pos_of_mac);
+    generated_source.push_str(&unhandled_source);
 
-    (left.to_string(), absolute_pos_of_mac, generated)
+    (left_of_mac.to_string(), absolute_pos_of_mac, generated_source)
 }
 
 fn handle_use_mocks<'a>(cx: &'a mut ExtCtxt, sp: Span, token_tree: &[TokenTree]) -> Box<MacResult + 'a> {
     use MacroInvocationPos::*;
 
+    // to parse the macros related to mock ussage the function is converted to string form
     let func_def: String = syntax::print::pprust::tts_to_string(token_tree);
     let mut reassembled = String::new();
     let mut remainder = func_def;
     let mut left: String;
 
+    // parse one macro a time then search for the next macro in the remaining string
     let BytePos(mut absolute_pos) = sp.lo;
+    let absolute_pos = absolute_pos as usize;
     while !remainder.is_empty() {
+
         match find_next_mock_macro_invocation(&remainder) {
             None => {
                 reassembled.push_str(&remainder);
                 remainder = String::new();
             },
             Some(invocation) => {
-                match invocation {
-                    NewMock(pos) => {
-                        let ret = prepare_handle_macro(&remainder, pos, absolute_pos as usize, handle_new_mock);
-                        left = ret.0;
-                        absolute_pos = ret.1 as u32;
-                        remainder = ret.2;
-                        reassembled.push_str(&left);
-                    },
-                    Given(pos) => {
-                        let ret = prepare_handle_macro(&remainder, pos, absolute_pos as usize, handle_given);
-                        left = ret.0;
-                        absolute_pos = ret.1 as u32;
-                        remainder = ret.2;
-                        reassembled.push_str(&left);
-                    },
-                    ExpectInteractions(pos) => {
-                        let ret = prepare_handle_macro(&remainder, pos, absolute_pos as usize, handle_expect_invocations);
-                        left = ret.0;
-                        absolute_pos = ret.1 as u32;
-                        remainder = ret.2;
-                        reassembled.push_str(&left);
-                    },
-                    ThenVerifyInteractions(pos) => {
-                        let ret = prepare_handle_macro(&remainder, pos, absolute_pos as usize, handle_then_verify_interactions);
-                        left = ret.0;
-                        absolute_pos = ret.1 as u32;
-                        remainder = ret.2;
-                        reassembled.push_str(&left);
-                    }
-                }
+                let (left, absolute_pos, right) = match invocation {
+                    NewMock(pos) => handle_macro(&remainder, pos, absolute_pos, handle_new_mock),
+                    Given(pos) => handle_macro(&remainder, pos, absolute_pos, handle_given),
+                    ExpectInteractions(pos) => handle_macro(&remainder, pos, absolute_pos, handle_expect_invocations),
+                    ThenVerifyInteractions(pos) => handle_macro(&remainder, pos, absolute_pos, handle_then_verify_interactions),
+                };
+
+                reassembled.push_str(&left);
+                remainder = right;
             }
         }
     }
 
+    println!("{}", reassembled);
+    // once all macro invocations have been removed from the string (and replaced with the actual mock code) it can be parsed back into a function item
     let maybe_fn = syntax::parse::parse_item_from_source_str("".to_owned(), reassembled, cx.parse_sess()).unwrap();
     let fn_: P<ast::Item> = maybe_fn.unwrap();
 
     let token_pairs = handle_generate_mocks();
+    for &(ref tok, ref toks) in token_pairs.iter() {
+        println!("{}", tok);
+        for t in toks {
+            println!("{}", t);
+        }
+    }
     let mut items = token_pairs.iter()
                                .map(|&(ref t, _)| {
                                    syntax::parse::parse_item_from_source_str(
@@ -187,8 +182,8 @@ fn handle_use_mocks<'a>(cx: &'a mut ExtCtxt, sp: Span, token_tree: &[TokenTree])
     items.push(fn_);
 
     let mut impls = token_pairs.iter()
-                               .map(|&(_, ref t)| {
-                                   println!("{:?}", t);
+                               .flat_map(|&(_, ref t)| t.into_iter())
+                               .map(|t| {
                                    syntax::parse::parse_item_from_source_str(
                                        "".to_owned(), t.to_string(), cx.parse_sess()
                                    ).unwrap().unwrap()
